fc4b390b15a30b263120c6457328ce9388e2f7da
unknown <ckmaina@gmail.com>
2020-07-13 15:22:06 +0200

    nodeweek4homework

---
+++ nodejs/week4/src/backend/api/meals.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const app = express();
+const router = express.Router();
+const knex = require("../database");
+
+router.get("/titles", async (request, response) => {
+  try {
+    // knex syntax for selecting things. Look up the documentation for knex for further info
+    const titles = await knex("meals").select("title");
+    response.json(titles);
+  } catch (error) {
+    throw error;
+  }
+});
+
+//Gets all meals
+router.get("/", async (req, res) => {
+  try {
+    const allmeals = await knex("meals").select("*");
+    res.json(allmeals);
+  } catch (error) {
+    throw error;
+  }
+});
+
+//Gets meal by id
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const find = await knex("meals").where({ id });
+    res.json(find);
+  } catch (error) {
+    throw error;
+  }
+});
+
+//posts a new object to the meals table
+router.post("/", async (req, res) => {
+  const { title, price } = req.body;
+  try {
+    knex
+      .insert(req.body)
+      .into("meals")
+      .then((data) => {
+        res.send("successful update");
+      });
+  } catch (error) {
+    error.send("Unable to save that entry into the database");
+  }
+});
+
+//PUT meals
+router.put("/:id", async (req, res) => {
+  try {
+    await knex("meals")
+      .update(req.body)
+      .where({ id: req.params.id })
+      .then(() => {
+        res.send("success");
+      });
+  } catch (error) {
+    throw error;
+  }
+});
+
+//delete
+router.delete("/:id", async (req, res) => {
+  const delMeal = knex("meals").where({ id: req.params.id }).del();
+  res.send("deleted");
+});
+
+module.exports = router;
---
+++ nodejs/week4/src/frontend/pages/meals.js
@@ -0,0 +1,37 @@
+const deleteMeal = (id) => {
+  fetch(`/api/meals/${id}`, {
+    method: "DELETE",
+  })
+    .then((res) => res.json())
+    .then((response) => {
+      console.log(response);
+    });
+};
+
+window.handleMealsRequest = () => {
+  document.body.innerHTML = `
+  <header>Meal Sharing app
+  <ul>
+  <a href="/" data-navigo>Home</a>
+   <a href="meals" data-navigo>Meals</a>
+    <a href="meal/1" data-navigo>Reservation</a>
+    <footer>Copyright@2020</footer>
+  </ul>
+  </header>
+
+    <h1>Meals</h1>`;
+
+  // make sure the backend api works before working with it here
+  fetch("/api/meals")
+    .then((response) => response.json())
+    .then((meals) => {
+      const ul = document.createElement("ul");
+      meals.forEach((meal) => {
+        const li = document.createElement("li");
+        li.innerHTML = `${meal.title} DKK ${meal.price} <a href="meal/${meal.id}"> </a>
+        <button "> <a href="meal/${meal.id}"> Details for ${meal.title}</a></button>`;
+        ul.appendChild(li);
+      });
+      document.body.appendChild(ul);
+    });
+};
